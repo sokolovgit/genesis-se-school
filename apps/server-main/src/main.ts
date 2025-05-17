a8c9b335d51a1d11c0a90ef76d59b366d96a4010
sokolovgit <sanok.sokolov928100@gmail.com>
2025-05-17 14:58:12 +0300

    fix(server-main): fix swagger docs setup logic

--- apps/server-main/src/main.ts
+++ apps/server-main/src/main.ts
@@ -1,37 +1,36 @@
-import { NestFactory } from "@nestjs/core"
-import { AppModule } from "./app.module"
-import { ConfigService } from "@nestjs/config"
-import { Logger, ValidationPipe } from "@nestjs/common"
-import { createDocument } from "./swagger"
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { createDocument } from './swagger';
 
 async function bootstrap() {
-  const logger = new Logger()
+  const logger = new Logger();
 
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
 
-  const config = app.get(ConfigService)
-  const port = config.get<string>("port")
-  
-  const isDocsEnabled = config.get<boolean>("docs.enabled")
+  const config = app.get(ConfigService);
+  const port = config.get<string>('port');
+
+  const isDocsEnabled = config.get<boolean>('docs.enabled');
 
   if (isDocsEnabled) {
-    createDocument(app)
+    createDocument(app);
   }
 
-  await app.listen(port)
-  
-  const appUrl = await app.getUrl()  
+  await app.listen(port);
 
+  const appUrl = await app.getUrl();
 
-  logger.log(`🚀 Application is running on: ${appUrl}`)
+  logger.log(`🚀 Application is running on: ${appUrl}`);
 
   if (isDocsEnabled) {
-    const docsPath = config.get<string>("docs.path")
-    logger.log(`📚 API Docs are available at: ${appUrl}/${docsPath}`)
+    const docsPath = config.get<string>('docs.path');
+    logger.log(`📚 API Docs are available at: ${appUrl}/${docsPath}`);
   }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-bootstrap()
+bootstrap();
